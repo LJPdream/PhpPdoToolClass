605b657938e66648c688b269152c16662969afca
LJPdream <984920281@qq.com>
2017-01-20 20:53:53 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php 
 define("DB_HOST",'localhost');
 define("DB_USER",'root');
-define('DB_PWD','null010');
-define('DB_NAME','info');
+define('DB_PWD','root');
+define('DB_NAME','test');
 define('DB_PORT','3306');
 define('DB_TYPE','mysql');
 define('DB_CHARSET','utf8');
